f02d604018aa6f1d03613028ebec4cc87d48c6b3
utsavdubey724 <55616400+utsavdubey724@users.noreply.github.com>
2020-10-10 14:39:59 +0530

    Add files via upload

--- Part3_Ques4.cpp
+++ Part3_Ques4.cpp
@@ -1,33 +1,22 @@
-
-#include <bits/stdc++.h>
-using namespace std;
-
-struct Point {
-	int x, y;
-};
-
-int overlappingArea(Point l1, Point r1, Point l2, Point r2)
-{
-
-	int area1 = abs(l1.x - r1.x) * abs(l1.y - r1.y);
-	int area2 = abs(l2.x - r2.x) * abs(l2.y - r2.y);
-	int areaI = (min(r1.x, r2.x) - max(l1.x, l2.x)) * (min(r1.y, r2.y) - max(l1.y, l2.y));
-
-	return (area1 + area2 - areaI);
-}
-
-int main()
-{
-	int n;
-	cin>>n;
-	int temp=n-1;
-	while(temp--){
-		Point l1,r1,l2,r2;
-		cin>>l1.x>>l1.y>>r1.x>>r1.y;
-		cin>>l2.x>>l2.y>>r2.x>>r2.y;	
-
-		cout << overlappingArea(l1, r1, l2, r2);
-	}
-	return 0;
-}
-
+#include<bits/stdc++.h>
+    using namespace std;
+    # define ll long long
+    int main(){
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        int t;
+        cin>>t;
+        int a[1000][1000]={0},i,j,res=0,x1,x2,y1,y2;
+        while(t--){
+            cin>>x1>>y1>>x2>>y2;
+            for(i=x1;i<x2;i++){
+                for(j=y1;j<y2;j++){
+                    if(a[i][j]==0){
+                        a[i][j]=1;
+                        res+=1;
+                    }
+                }
+            }
+        }
+        cout<<res;
+    }
